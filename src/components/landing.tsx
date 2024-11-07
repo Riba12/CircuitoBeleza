7723de50a3b664726e3dcaf4f2819ae3ceea7101
RomuloRibeiroSenai <romulo.s.ribeiro@aluno.senai.br>
2024-11-07 15:23:07 -0300

    media update

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -10,7 +10,7 @@ export default function Landing() {
         <>
             <div className="bg-bege-100 flex">
                 <div className="grow "></div>
-                <section className="flex flex-row font-medium w-full h-[32rem] md:w-[960px] md:h-dvh ">
+                <section className="flex flex-row font-medium w-full h-[32rem] md:w-[960px] md:h-dvh 2xl:w-[1440px]">
                     {/* <div className="h-4/5  flex justify-center space-x-40">
                     <div className="flex flex-col justify-center w-14 md:w-1/3 z-10">
                         <div className="flex justify-end mt-8">
@@ -43,17 +43,17 @@ export default function Landing() {
                     </div>
                     <div className="w-1/2 flex flex-col md:justify-evenly">
                         <div className="animate-slideInLogo w-24 md:animate-slideInLogoBig md:w-44">
-                            <Image src={logo} alt="Logo" className="w-12 md:w-32" />
+                            <Image src={logo} alt="Logo" className="w-12 md:w-20 2xl:w-32" />
                         </div>
-                        <div className="animate-slideInText w-4/5 md:text-4xl">
+                        <div className="animate-slideInText w-4/5 md:text-xl 2xl:text-4xl">
                             Centro técnico de capacitação e aperfeiçoamento profissional.
                         </div>
-                        <div className="animate-slideInText w-4/5 md:text-4xl">
+                        <div className="animate-slideInText w-4/5 md:text-xl 2xl:text-4xl">
                             Eleve sua carreira na área da beleza com técnicas práticas e conteúdos dinâmicos de alta qualidade que você precisa para ir mais longe.
                         </div>
-                        <div className="animate-slideInBT w-4/5 mt-10">
+                        <div className="animate-slideInBT w-4/5 mt-10 md:mt-0">
                             <Link href="https://pay.kiwify.com.br/fTT4C1K" passHref legacyBehavior >
-                                <a target='_blank' className="w-72 p-4 rounded-full text-center bg-laranja-100 text-white text-xl md:text-3xl hover:bg-gray-400">
+                                <a target='_blank' className="w-72 p-4 rounded-full text-center bg-laranja-100 text-white text-xl md:text-2xl 2xl:text-3xl hover:bg-gray-400">
                                     Saber mais
                                 </a>
                             </Link>
