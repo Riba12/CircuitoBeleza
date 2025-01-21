d3818c164aae909c86307ba7ba2e6adf84838671
RomuloRibeiroSenai <romulo.s.ribeiro@aluno.senai.br>
2025-01-21 17:45:51 -0300

    Pixel ID change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '506961385431018'); 
+              fbq('init', '2548576852200271'); 
               fbq('track', 'PageView');
             `,
           }}
